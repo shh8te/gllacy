ecb800ec43049b054fc307ea40e0917cc6ed6632
shh8te <31075598+shh8te@users.noreply.github.com>
2017-11-27 20:52:00 +0300

    fghj
    DFGJK

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('page is loaded');
+function initMap() {
+  var gllacy = {lat: 59.938536, lng: 30.322879};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: gllacy
+  });
+  var marker = new google.maps.Marker({
+    position: gllacy,
+    map: map,
+    icon: 'img/map_pin.png'
+  });
+};
+initMap();
