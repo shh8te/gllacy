a6646bab7f7ba8610d8888e93b32b79522cfa580
shh8te <31075598+shh8te@users.noreply.github.com>
2017-12-11 23:51:00 +0300

    fix
    pseudo placeholder

--- js/script.js
+++ js/script.js
@@ -1,13 +1,44 @@
+var emailFeedback = document.querySelector('.subscribe-email input[type="text"]');
+
+emailFeedback.addEventListener('focus', function(e) {
+	this.classList.add('active');
+});
+
+emailFeedback.addEventListener('blur', function(e) {
+	if (this.value.length > 0) {
+		return;
+	}
+
+	this.classList.remove('active');
+});
+
+var search = document.querySelector('.main-header-user_search .search-form input');
+
+search.addEventListener('focus', function(e) {
+	this.classList.add('active');
+});
+
+search.addEventListener('blur', function(e) {
+	if (this.value.length > 0) {
+		return;
+	}
+
+	this.classList.remove('active');
+});
+
 function initMap() {
-  var gllacy = {lat: 59.938536, lng: 30.322879};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 16,
-    center: gllacy
-  });
-  var marker = new google.maps.Marker({
-    position: gllacy,
-    map: map,
-    icon: 'img/map_pin.png'
-  });
+	var gllacy = {
+		lat: 59.938536,
+		lng: 30.322879
+	};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 16,
+		center: gllacy
+	});
+	var marker = new google.maps.Marker({
+		position: gllacy,
+		map: map,
+		icon: 'img/map_pin.png'
+	});
 };
 initMap();
